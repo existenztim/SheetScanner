07b3a5edcf60ea162fcd1b904e96f75a39f5f81a
Tim Sundell <tim920129@hotmail.com>
2024-01-13 12:22:47 +0100

    :bento: Added missing page/route structure

---
+++ src/app/(routes)/notes/[userId]/[noteId]/page.tsx
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+  <p>Gör en noteEditor komp</p>
+  )};
+
+export default page;
---
+++ src/app/(routes)/notes/page.tsx
@@ -0,0 +1,7 @@
+
+
+const page = () => {
+  return <p>Denna page kan tas bort, alt så kommer den visas när man ej är inloggad</p>;
+};
+
+export default page;
